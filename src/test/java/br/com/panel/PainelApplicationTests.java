77f7018682a07e8877e5da3d2930e842b892d26e
p000059 <marcelo.acad@gmail.com>
2021-12-13 10:47:15 -0300

    20211213104700

---
+++ src/test/java/br/com/panel/PainelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
