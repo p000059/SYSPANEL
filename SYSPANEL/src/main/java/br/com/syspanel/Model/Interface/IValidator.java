a4da2c5b0798826097232140c5518437c5bc5828
p000059 <marcelo.acad@gmail.com>
2021-12-15 21:24:44 -0300

    delete

--- SYSPANEL/src/main/java/br/com/syspanel/Model/Interface/IValidator.java
+++
@@ -1,7 +0,0 @@
-package br.com.syspanel.Model.Interface;
-import javax.xml.bind.ValidationException;
-
-public interface IValidator<T> {
-    
-    void valida(T object) throws ValidationException;
-}
